c2282abc4a4fc8e12e080e48d37a59388413fe09
GuavaMZ <guava.mzco@gmail.com>
2025-06-25 20:40:00 +0300

    25-6-2025

--- js/pages/clients.js
+++ js/pages/clients.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const searchContent = async() => {
         try {
-            const fetchPromises = pagesToSearchThrough.map(page => fetch(page).then(response => {
+            const content = await pagesToSearchThrough.map(page => fetch(page).then(response => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
