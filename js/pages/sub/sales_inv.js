df44b80fcf6b875f429711e0407046e42407030d
GuavaMZ <guava.mzco@gmail.com>
2025-06-30 18:39:45 +0300

    30-6-2025

--- js/pages/sub/sales_inv.js
+++ js/pages/sub/sales_inv.js
@@ -27,14 +27,91 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-     document.addEventListener('click', (event) => {
+    document.addEventListener('click', (event) => {
         if ((!branchSelectionButton.contains(event.target) && !branchSelectionMenuItems.contains(event.target))) {
             branchSelectionMenuItems.style.display = 'none';
             branchSelectionButton.setAttribute('aria-expanded', 'false');
         }
     });
 
-    
+    const invDateInput = document.getElementById('inv-date');
+
+    invDateInput.value = new Date().toISOString().split('T')[0];
+    flatpickr("#inv-date", {
+
+    });
+
+    const paymentSelectionButton = document.getElementById('payment-method-select-button');
+    const paymentSelectionMenuItems = document.getElementById('payment-method-select-menu');
+    const paymentSelectionItem = document.querySelectorAll('.payment-method-select-item');
+
+    paymentSelectionButton.addEventListener('click', () => {
+        const isExpanded = paymentSelectionButton.getAttribute('aria-expanded') === 'true';
+        paymentSelectionButton.setAttribute('aria-expanded', !isExpanded);
+
+        if (paymentSelectionMenuItems.style.display === 'none') {
+            paymentSelectionMenuItems.style.display = 'block'; // Show the dropdown
+        } else {
+            paymentSelectionMenuItems.style.display = 'none'; // Hide the dropdown
+        }
+    });
+
+    paymentSelectionItem.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault(); // Prevent default link behavior (full page reload)
+
+            // Hide the dropdown menu after an item is clicked
+            paymentSelectionMenuItems.style.display = 'none';
+            paymentSelectionButton.setAttribute('aria-expanded', 'false');
+
+            // Optionally, update the button text to show selected item
+            paymentSelectionButton.childNodes[0].nodeValue = item.textContent + ' '; // Update button text
+        });
+    });
+
+    document.addEventListener('click', (event) => {
+        if ((!paymentSelectionButton.contains(event.target) && !paymentSelectionMenuItems.contains(event.target))) {
+            paymentSelectionMenuItems.style.display = 'none';
+            paymentSelectionButton.setAttribute('aria-expanded', 'false');
+        }
+    });
+
+
+    const priceTypeSelectionButton = document.getElementById('price-type-select-button');
+    const priceTypeSelectionMenuItems = document.getElementById('price-type-select-menu');
+    const priceTypeSelectionItem = document.querySelectorAll('.price-type-select-item');
+
+    priceTypeSelectionButton.addEventListener('click', () => {
+        const isExpanded = priceTypeSelectionButton.getAttribute('aria-expanded') === 'true';
+        priceTypeSelectionButton.setAttribute('aria-expanded', !isExpanded);
+
+        if (priceTypeSelectionMenuItems.style.display === 'none') {
+            priceTypeSelectionMenuItems.style.display = 'block'; // Show the dropdown
+        } else {
+            priceTypeSelectionMenuItems.style.display = 'none'; // Hide the dropdown
+        }
+    });
+
+    priceTypeSelectionItem.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault(); // Prevent default link behavior (full page reload)
+
+            // Hide the dropdown menu after an item is clicked
+            priceTypeSelectionMenuItems.style.display = 'none';
+            priceTypeSelectionButton.setAttribute('aria-expanded', 'false');
+
+            // Optionally, update the button text to show selected item
+            priceTypeSelectionButton.childNodes[0].nodeValue = item.textContent + ' '; // Update button text
+        });
+    });
+
+    document.addEventListener('click', (event) => {
+        if ((!priceTypeSelectionButton.contains(event.target) && !priceTypeSelectionMenuItems.contains(event.target))) {
+            priceTypeSelectionMenuItems.style.display = 'none';
+            priceTypeSelectionButton.setAttribute('aria-expanded', 'false');
+        }
+    });
+
     // const salesTable = document.getElementById("sales-table");
     // const salesData = [
     //     {
